d8e663a0ed13341ec5290423a2de483dd4ae2d16
wangjunasd <wj00@qq.com>
2017-09-23 19:15:49 +0800

    update

--- functions.php
+++ functions.php
@@ -17,3 +17,30 @@ function binToNum($bin){
     
     return $hexAry[1];
 }
+
+function makeCloseMessage($fd){
+    $message=pack('H*','abacadae74');
+    $message.=pack('N',$fd);
+    $message.="\n";
+    return $message;
+}
+
+function makeConnectMessage($fd){
+    $message=pack('H*','abacadae73');
+    $message.=pack('N',$fd);
+    $message.="\n";
+    return $message;
+}
+
+function makeSendMessage($fd,$data){
+    $message=pack('H*','abacadae72');
+    $message.=pack('N',$fd);
+    $message.=$data;
+    return $message;
+}
+
+function makeBoardcastMessage($data){
+    $message=pack('H*','abacadae75');
+    $message.=$data;
+    return $message;
+}
--- server.php
+++ server.php
@@ -11,71 +11,156 @@ $serv->set(array(
     'heartbeat_idle_time' => 120
 ));
 
-
 $serv->session = new swoole_table(16);
 
-$serv->session->column('fd', swoole_table::TYPE_INT, 4);
 $serv->session->column('ip', swoole_table::TYPE_STRING, 15);
 $serv->session->column('connectTime', swoole_table::TYPE_INT, 4);
 $serv->session->column('activeTime', swoole_table::TYPE_INT, 4);
 
 $serv->session->create();
 
-$serv->on('workerstart', function ($serv, $id) {
-
-});
+$serv->on('workerstart', function ($serv, $id) {});
 
 $serv->on('connect', function ($serv, $fd) {
-
-});
-$serv->on('receive', function ($serv, $fd, $from_id, $data) {
-    if(binTohex(substr($data, 0,4))==="abacadae"){
-        //指令
-        switch (binTohex(substr($data, 4,1))){
-            case "71":
-             //注册   
-                $fdinfo = $serv->connection_info($fd);
-                $serv->session->set(count($serv->session),array(
-                    'fd'=>$fd,
-                    'ip'=> $fdinfo['remote_ip'],
-                    'connectTime'=>time(),
-                    'activeTime'=>time()
-                ));
-               break;
-            case "72":
-             //发送消息
-                $sendFd = binToNum(substr($data, 5,4));
-                
-                if ($serv->exist($sendFd)){
-                    
-                    
-                    
-                }
-                
-             break;
-            
-            
-            
-            
-            
+    if (count($serv->session)){
+        
+        //get client fd
+        $counter=count($serv->session);
+        
+        $selected=mt_rand(0, $counter-1);
+        
+        $i=0;
+        
+        foreach ($serv->session as $clientfd=>$clientInfo){
+        
+            if ($selected===$i){
+                $serv->send($clientfd,makeConnectMessage($fd));
+        
+                break;
+            }
+            $i++;
         }
         
         
-        
     }else{
-        //普通请求，转发
-        
-        
-        
-        
-        
-        
+        //dont do nothing
     }
+});
+$serv->on('receive', function ($serv, $fd, $from_id, $data) {
     
-    
+    if (binTohex(substr($data, 0, 4)) === "abacadae") {
+        // 指令
+        switch (binTohex(substr($data, 4, 1))) {
+            case "71":
+                // 注册
+                $fdinfo = $serv->connection_info($fd);
+                $serv->session->set($fd, array(
+                    'ip' => $fdinfo['remote_ip'],
+                    'connectTime' => time(),
+                    'activeTime' => time()
+                ));
+                break;
+            case "72":
+                // 发送消息
+                $sendFd = binToNum(substr($data, 5, 4));
+                
+                if ($serv->exist($sendFd)) {
+                    if ($fd!=$sendFd)
+                     $serv->send($sendFd, substr($data, 9));
+                } else {
+                    // 告知客户端关闭这个连接
+                    
+                    $serv->send($fd, makeCloseMessage($sendFd));
+                }
+                
+                break;
+            
+            case "73":
+                // connect
+                
+                // 服务端不处理此消息
+                
+                break;
+            
+            case "74":
+                // close
+                $sendFd = binToNum(substr($data, 5, 4));
+                
+                if ($serv->exist($sendFd) && $fd!=$sendFd) {
+                    
+                    $serv->close($sendFd);
+                }
+                
+                break;
+            
+            case "75":
+                // boardcast
+                
+                $message = substr($data, 5);
+                
+                $start_fd = 0;
+                while (true) {
+                    $conn_list = $serv->connection_list($start_fd, 10);
+                    if ($conn_list === false or count($conn_list) === 0) {
+                        break;
+                    }
+                    $start_fd = end($conn_list);
+                    
+                    foreach ($conn_list as $curFd) {
+                        if ($fd != $curFd)
+                            $serv->send($curFd, $message);
+                    }
+                }
+                
+                break;
+        }
+    } else {
+        // 普通请求，转发
+        
+        //get client fd
+        $counter=count($serv->session);
+        
+        $selected=mt_rand(0, $counter-1);
+        
+        $i=0;
+        
+        foreach ($serv->session as $clientfd=>$clientInfo){
+            
+            if ($selected===$i){
+                $serv->send($clientfd,makeSendMessage($fd, $data));
+                
+                break;
+            }
+            $i++;
+        }
+    }
 });
 
 $serv->on('close', function ($serv, $fd) {
+    
+    if (count($serv->session)){
+    
+        //get client fd
+        $counter=count($serv->session);
+    
+        $selected=mt_rand(0, $counter-1);
+    
+        $i=0;
+    
+        foreach ($serv->session as $clientfd=>$clientInfo){
+    
+            if ($selected===$i){
+                $serv->send($clientfd,makeConnectMessage($fd));
+    
+                break;
+            }
+            $i++;
+        }
+    
+    
+    }else{
+        //dont do nothing
+    }
     
     // update session table.
     if ($serv->session->exist($fd)) {
